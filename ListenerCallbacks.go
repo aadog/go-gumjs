eb3904c7cd920dd5c040ac21c97134fdb5b4ae46
a <a@q.com>
2024-02-20 14:42:12 +0800

    update

--- ListenerCallbacks.go
+++ ListenerCallbacks.go
@@ -3,8 +3,10 @@ package gumjs
 import "sync"
 
 type IListenerCallback interface {
-	GetVal(key string) any
+	MustGetVal(key string) any
+	GetVal(key string) (any, bool)
 	SetVal(key string, val any)
+	HasVal(key string) bool
 	GetOnEnter() func(cb IListenerCallback, context *InvocationContext)
 	GetOnLeave() func(cb IListenerCallback, context *InvocationContext)
 }
@@ -15,9 +17,17 @@ type InvocationListenerCallbacks struct {
 	OnLeave func(cb IListenerCallback, context *InvocationContext)
 }
 
-func (i *InvocationListenerCallbacks) GetVal(key string) any {
+func (i *InvocationListenerCallbacks) HasVal(key string) bool {
+	_, b := i.val.Load(key)
+	return b
+}
+func (i *InvocationListenerCallbacks) MustGetVal(key string) any {
 	v, _ := i.val.Load(key)
 	return v
+}
+func (i *InvocationListenerCallbacks) GetVal(key string) (any, bool) {
+	v, b := i.val.Load(key)
+	return v, b
 }
 func (i *InvocationListenerCallbacks) SetVal(key string, val any) {
 	i.val.Store(key, val)
